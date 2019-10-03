2a71a55c7433cd638e6e3284ec2b4f0fb9d00f59
Aldihamda <aldihamda94@gmail.com>
2019-10-03 12:23:17 +0700

    Bab 4

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
@@ -3,8 +3,17 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import id.ac.polinema.intent.model.User;
 
 public class ProfileParcelableActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
+
+    public static final String USER_KEY = "user";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,11 +21,21 @@ public class ProfileParcelableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_parcelable);
 
         // TODO: bind here
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-
             // TODO: display value here
+            User user = getIntent().getParcelableExtra(USER_KEY);
+            String username = user.getUsername();
+            String name = user.getName();
+            int age = user.getAge();
+
+            usernameText.setText(username);
+            nameText.setText(name);
+            ageText.setText(String.valueOf(age));
         }
     }
 }
